7d86b60cdb857f8f976b16937ecd9837d437f068
OlgaNiem <olhaniemiets@gmail.com>
2024-09-13 12:42:06 +0300

    update

---
+++ src/card.tsx
@@ -0,0 +1,29 @@
+import { GameCard } from "./types";
+import React from "react";
+type CardProps = {
+    card: GameCard;
+    isFlipped: boolean;
+    isDisabled: boolean;
+    onFlip(GameCard: GameCard) : void
+}
+
+export function Card ({ card, isFlipped, isDisabled, onFlip}: CardProps) {
+    const handleClick = React.useCallback(() => {
+        onFlip(card)
+    }, [onFlip, card])
+
+    const isCardFlipped = card.isMatched || isFlipped
+
+    return (
+        <button className={`flex justify-center border-2 border-purple-950 p-10 m-2 w-1/5
+            ${isCardFlipped ? "bg-green-200" : "bg-red-200"}`
+        }
+        disabled={isCardFlipped || isDisabled}
+        onClick={handleClick}>
+        
+        {isCardFlipped ? card.value : "?"}
+
+        </button>
+    )
+
+}
