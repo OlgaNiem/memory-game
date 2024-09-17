2af355577b54795285efc3cfe7239b75dba5bb8a
OlgaNiem <olhaniemiets@gmail.com>
2024-09-17 12:32:48 +0300

    update

--- src/card.tsx
+++ src/card.tsx
@@ -1,29 +1,30 @@
 import { GameCard } from "./types";
 import React from "react";
+
 type CardProps = {
-    card: GameCard;
-    isFlipped: boolean;
-    isDisabled: boolean;
-    onFlip(GameCard: GameCard) : void
-}
+  card: GameCard;
+  isFlipped: boolean;
+  isDisabled: boolean;
+  onFlip(GameCard: GameCard): void;
+};
 
-export function Card ({ card, isFlipped, isDisabled, onFlip}: CardProps) {
-    const handleClick = React.useCallback(() => {
-        onFlip(card)
-    }, [onFlip, card])
+export function Card({ card, isFlipped, isDisabled, onFlip }: CardProps) {
+  const handleClick = React.useCallback(() => {
+    if (!isFlipped && !isDisabled && !card.isMatched) {
+      onFlip(card);
+    }
+  }, [onFlip, card, isFlipped, isDisabled]);
 
-    const isCardFlipped = card.isMatched || isFlipped
+  const isCardFlipped = card.isMatched || isFlipped;
 
-    return (
-        <button className={`flex justify-center border-2 border-purple-950 p-10 m-2 w-1/5
-            ${isCardFlipped ? "bg-green-200" : "bg-red-200"}`
-        }
-        disabled={isCardFlipped || isDisabled}
-        onClick={handleClick}>
-        
-        {isCardFlipped ? card.value : "?"}
-
-        </button>
-    )
-
+  return (
+    <button
+      className={`flex justify-center border-2 border-purple-950 p-10 m-2 w-1/5
+        ${isCardFlipped ? "bg-green-200" : "bg-red-200"}`}
+      disabled={isCardFlipped || isDisabled}
+      onClick={handleClick}
+    >
+      {isCardFlipped ? card.value : "?"}
+    </button>
+  );
 }
